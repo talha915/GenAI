1412c1ec7e41a69c13b16dc08b8855242d6977f3
talha915 <talhazafa915426@gmail.com>
2025-06-22 19:18:50 +0200

    added read file method

--- langchain_llm.py
+++ langchain_llm.py
@@ -1,9 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.document_loaders import DirectoryLoader
 
-loader = DirectoryLoader(path="./documents", glob="*.pdf", loader_cls=PyPDFLoader)
 # file_path = "filename.pdf"
 # loader = PyPDFLoader(file_path)
 
-documents = loader.load()
-print(documents)
+def load_files(folder_path):
+    try:
+        loader = DirectoryLoader(path=folder_path, glob="*.pdf", loader_cls=PyPDFLoader)
+        documents = loader.load()
+        return documents
+    except Exception as e:
+        return e    
+    
+file_content = load_files('./documents')
+print(f"Here is collective file content: {file_content}")    
