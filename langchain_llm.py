08bb4c1f5126922ea2a75ec29a8f131ffe248e47
talha915 <talhazafa915426@gmail.com>
2025-06-22 19:48:37 +0200

    added env

--- langchain_llm.py
+++ langchain_llm.py
@@ -1,5 +1,17 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+# Get parent directory 
+env_path = Path(__file__).resolve().parent / '.env'
+
+# Load .env from parent directory
+load_dotenv(dotenv_path=env_path)
+huggingface_api_token = os.getenv("huggingface_api_token")
+grok_api_token = os.getenv("grok_api_key")
 
 # file_path = "filename.pdf"
 # loader = PyPDFLoader(file_path)
